d271c573fce335fba114a99a52d86dbe728c1418
Shekhar <shekhar59324@gmail.com>
2020-04-17 21:47:06 +0530

    Initial commit- total change request

--- app/src/main/java/com/example/databaseexample/MainActivity.java
+++ app/src/main/java/com/example/databaseexample/MainActivity.java
@@ -18,93 +18,17 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-    private DatabaseHelper myHelper;
-
-    private ListView listView;
-
-    private SimpleCursorAdapter adapter;
-
-    final String[] from = new String[]{DatabaseHelper._ID,
-            DatabaseHelper.NAME, DatabaseHelper.ADDRESS, DatabaseHelper.PAN};
-
-    final int[] to = new int[]{R.id.id, R.id.name, R.id.address, R.id.pan};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_emp_list);
 
-        myHelper = new DatabaseHelper(this);
-        myHelper.open();
 
-        listView = (ListView) findViewById(R.id.list_view);
-        listView.setEmptyView(findViewById(R.id.empty));
 
-        Cursor c = myHelper.getAllEmployees();
 
-        adapter = new SimpleCursorAdapter(this, R.layout.activity_view_record, c, from, to, 0);
-
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                TextView idTextView = (TextView) view.findViewById(R.id.id);
-                TextView nameTextView = (TextView) view.findViewById(R.id.name);
-                TextView addressTextView = (TextView) view.findViewById(R.id.address);
-                TextView panTextView = (TextView) view.findViewById(R.id.pan);
-
-                String id = idTextView.getText().toString();
-                String title = nameTextView.getText().toString();
-                String desc = addressTextView.getText().toString();
-                String pant = panTextView.getText().toString();
-
-                Intent modify_intent = new Intent(getApplicationContext(), ModifyEmployeeActivity.class);
-                modify_intent.putExtra("name", title);
-                modify_intent.putExtra("address", desc);
-                modify_intent.putExtra("pan", pant);
-                modify_intent.putExtra("id", id);
-
-                startActivity(modify_intent);
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-
-        MenuItem item = menu.findItem(R.id.search);
-        SearchView searchview = (SearchView) item.getActionView();
-
-        searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                adapter.getFilter().filter(newText);
-                return false;
-            }
-        });
-        return true;
     }
 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-        if (id == R.id.add_record) {
-            Intent add_mem = new Intent(this, AddEmployeeActivty.class);
-            startActivity(add_mem);
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
 }
